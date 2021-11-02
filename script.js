d0738eae628a4c02f48cf624e90083c6326af885
Vitaly <80334205+kViTwIm@users.noreply.github.com>
2021-11-02 21:45:07 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+var img1 = document.getElementById("img1");
+img1.addEventListener("click", open1);
+var img2 = document.getElementById("img2");
+img2.addEventListener("click", open2);
+var img3 = document.getElementById("img3");
+img3.addEventListener("click", open3);
+var img4 = document.getElementById("img4");
+img4.addEventListener("click", open4);
+var img5 = document.getElementById("img5");
+img5.addEventListener("click", open5);
+var img6 = document.getElementById("img6");
+img6.addEventListener("click", open6);
+var img7 = document.getElementById("img7");
+img7.addEventListener("click", open7);
+var img8 = document.getElementById("img8");
+img8.addEventListener("click", open8);
+
+function open1(event){
+    window.open("index_1.html","_self",false);
+}
+function open2(event){
+    window.open("index_2.html","_self",false);
+}
+function open3(event){
+    window.open("index_3.html","_self",false);
+}
+function open4(event){
+    window.open("index_4.html","_self",false);
+}
+function open5(event){
+    window.open("index_5.html","_self",false);
+}
+function open6(event){
+    window.open("index_6.html","_self",false);
+}
+function open7(event){
+    window.open("index_7.html","_self",false);
+}
+function open8(event){
+    window.open("index_8.html","_self",false);
+}
